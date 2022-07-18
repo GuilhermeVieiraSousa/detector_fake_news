af4d24019c32f73a1a0e818bc0ac32ea6c067661
GianluccaCampana <gianluccacampana19@gmail.com>
2022-07-17 23:33:49 -0300

    validação do cadastro feita, ainda falta revisar a detecção

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from flask import Flask, render_template, session, request, redirect, url_for
 # request serve para a função saber se vai usar o método get ou post
 # redirect vai verificar se a sessão é válida
 # urk_for chama as funções definidas
-
+import re #expressão regular
 import pandas as pd
 import joblib
+import pickle
 
 from flask_mysqldb import MySQL 
 import MySQLdb.cursors 
@@ -46,14 +47,22 @@ def cadastro():
         cursor.execute('SELECT * FROM usuario WHERE email = % s', (email, ))
         account = cursor.fetchone() #guarda resultado encontrado
         if account: #caso já exista o cadastro do e-mail
-            msg = 'E-mail já cadastrado !'      
+            msg = 'E-mail já cadastrado !'
+        elif re.match(r'[0-9]+', nome):
+            msg='Nome de usuário não pode conter números'
+        elif not  re.match(r'[A-Za-z0-9]+', senha) :
+            msg='Senha não atende aos requisitos mínimos'       
+        
+        elif  len(senha) <8:
+            msg='Senha precisa ter 8 caracteres ou mais'        
         else: 
             #executando comando de inserção
             cursor.execute('INSERT INTO usuario(nome, senha, email) VALUES (% s, % s, % s)', (nome, senha, email, )) 
             mysql.connection.commit() #gravando a informação no banco
             msg = 'Conta registrada'
             return render_template('cadastro.html', msg=msg)  
-        
+    else:
+        return render_template('cadastro.html', msg=msg)
     return render_template('cadastro.html', msg=msg) 
     
 @app.route("/login", methods=['POST', 'GET'])
@@ -173,14 +182,15 @@ def analisando():
 
     #carregando arquico pkl
     analise = joblib.load("model_lr.pkl")
-
-    # Get values through input bars
-    noticia = request.form["areNoticia"]
     
-    # Put inputs to dataframe
-    X = pd.DataFrame([noticia], columns = ["preprocessed_news"])
+
+    # pegando valor pelo input
+    noticia = request.form["areaNoticia"]
+    
+    # criando dataframe
+    X = pd.DataFrame([[noticia]], columns = ["noticias"])
         
-    # Get prediction
+    # fazendo analise preditiva
     previsao = analise.predict(X)[0]      
     
    
